961ac51c87ceaa9c8966fc03ab75b06597d8ad9f
Renan Z <zumpchiat@gmail.com>
2022-09-07 18:55:07 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1> olá mundo</h1>
-      <NotificationButton />
+
+      <Header />
+
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"></div>
+          <SalesCard />
+        </section>
+
+      </main>
+
 
 
     </>
