cec3e9c0b5e2172ff2334b750ad02644333b787d
Renan Z <zumpchiat@gmail.com>
2022-09-08 16:27:46 -0300

    API basic

---
+++ backend/src/main/java/com/alpha/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.alpha.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alpha.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/alpha/dsmeta/services/SaleService.java
@@ -0,0 +1,23 @@
+package com.alpha.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alpha.dsmeta.entities.Sale;
+import com.alpha.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+
+	public List<Sale> findSales() {
+
+		return repository.findAll();
+
+	}
+
+}
